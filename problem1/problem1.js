edc43df0fea5558ec9d476d939673f64a2307db9
mwbit <mwbitt@gmail.com>
2018-04-06 22:48:27 -0300

    teste mint

--- problem1/problem1.js
+++ problem1/problem1.js
@@ -1,5 +1,4 @@
 function solution(num) {
-
     let number = num
     let counter = 0
     let max = 0
@@ -15,7 +14,6 @@ function solution(num) {
         } else if(counter >= 0) {
             counter++
         }
-
         number = parseInt(number / 2)
     }
     return max
